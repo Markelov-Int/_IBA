16312c43040a2d237f03ed9a93176b737b3d3bae
Markelov-Int <63475405+Markelov-Int@users.noreply.github.com>
2021-02-17 10:44:28 +0300

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,6 @@ var middleware = require("../middleware");
 
 //INDEX - show all campg
 router.get("/", function(req, res){
-
  //Get all camp from DB
  Campground.find({}, function(err, allCampgrounds){
  	if(err){
@@ -14,28 +13,20 @@ router.get("/", function(req, res){
 		res.render("index", {campgrounds:allCampgrounds});
 	}
  });
- 
 });
-
-
 
 
 //SHOW - show more info about one campg
 router.get("/:id" , function(req, res){
 	//find the campg
-	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+	Campground.findByIdAndUpdate(req.params.id,{$inc: {views: +1}}, {new:true}).populate("comments").exec(function(err, foundCampground){
       if(err){
 		console.log(err);
 	} else {
 		console.log(foundCampground)
-		res.render("campgrounds/show", {campground: foundCampground});
-	    Campground.findByIdAndUpdate(req.params.id,{$inc: {views: 1}}, {new:true})
-        .then((data)=>{console.log(data.views)})
+		res.render("campgrounds/show", {campground: foundCampground});  
 	}
 	});	
 });
-
-  
-
 
 module.exports = router;
